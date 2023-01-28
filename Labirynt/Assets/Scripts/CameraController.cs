72330fb7ad2d6962f7f99391137904d81643e05a
Prz_Kowalski <Mrsledzik@gmail.com>
2023-01-28 11:50:54 +0100

    first commit

---
+++ Labirynt/Assets/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] float mouseSenivity = 100f;
+    Transform playerBody;
+    float xRotation = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerBody = transform.parent;
+        //To wyłącza kursor myszki w naszej grze
+        Cursor.lockState = CursorLockMode.Locked;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSenivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSenivity * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
